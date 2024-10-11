d687e94663988e67c1f640115f4848619a07f51d
Harriet2004 <s3933558@student.rmit.edu.au>
2024-10-11 21:18:47 +1100

    Have seperate main.cpp for both bestfit and firstfit

---
+++ bestfit_main.cpp
@@ -0,0 +1,22 @@
+#include "memory_allocator.h"
+#include <cstdio>
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        printf("Usage: bestfit <datafile>\n");
+        return EXIT_FAILURE;
+    }
+
+    set_allocation_strategy(BEST_FIT); // Set strategy to BEST_FIT
+    process_datafile(argv[1]);
+
+    if (!error_occurred)
+    {
+        print_allocated_list();
+        print_free_list();
+    }
+
+    return EXIT_SUCCESS;
+}
